d5dbaf8ca4789fb4bc15138c090598816bf8cacf
nemi17 <nemi17@users.noreply.github.com>
2018-11-19 12:29:50 -0500

    Changed to new file.

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter a score:");
+        String userInput = console.nextLine();
+
+        int gradeA;
+        gradeA = (100-93);
+        int gradeAMinus;
+        gradeAMinus = (92-90);
+        int gradeBPlus;
+        gradeBPlus = (89-87);
+        int gradeB;
+        gradeB = (86-83);
+    }
+}
