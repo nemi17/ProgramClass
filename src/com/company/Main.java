7f20c32451a2a4db489f54d3dca1671f6ed1e09f
nemi17 <nemi17@users.noreply.github.com>
2018-11-19 14:08:16 -0500

    Working On

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int gradeA;
+        gradeA = (100 - 93);
+        int gradeAMinus;
+        gradeAMinus = (92 - 90);
+        int gradeBPlus;
+        gradeBPlus = (89 - 87);
+        int gradeB;
+        gradeB = (86 - 83);
+        int gradeBMinus;
+        gradeBMinus = (82 - 80);
+        int gradeCPlus;
+        gradeCPlus = (79 - 77);
+        int gradeC;
+        gradeC = (76 - 73);
+        int gradeCMinus;
+        gradeCMinus = (72 - 70);
+        int gradeDPlus;
+        gradeDPlus = (69 - 67);
+        int gradeD;
+        gradeD = (66 - 63);
+        int gradeDMinus;
+        gradeDMinus = (62 - 60);
+        int gradeF;
+        gradeF = (60 - 0);
+        int i;
+
         Scanner console = new Scanner(System.in);
         System.out.println("Enter a score:");
+        i = userInput.Nextint();
         String userInput = console.nextLine();
 
-        int gradeA;
-        gradeA = (100-93);
-        int gradeAMinus;
-        gradeAMinus = (92-90);
-        int gradeBPlus;
-        gradeBPlus = (89-87);
-        int gradeB;
-        gradeB = (86-83);
+        }
     }
 }
